125bb7ab374effcc913e9b1882ed43fd2872ec13
kishorek0344 <156921551+kishorek0344@users.noreply.github.com>
2024-08-26 09:33:13 +0530

    Update BannerProduct.js

--- src/components/BannerProduct.js
+++ src/components/BannerProduct.js
@@ -5,11 +5,10 @@ import img3 from '../img/ban de 3.png'
 import img4 from '../img/ban de 4.png'
 
 
-import image1Mobile from '../assest/banner/img1_mobile.jpg'
-import image2Mobile from '../assest/banner/img2_mobile.webp'
-import image3Mobile from '../assest/banner/img3_mobile.jpg'
-import image4Mobile from '../assest/banner/img4_mobile.jpg'
-import image5Mobile from '../assest/banner/img5_mobile.png'
+import image1 from '../img/ban mo1.png'
+import image2 from '../img/ban mo 2.png'
+import image3 from '../img/ban mo 3.png'
+import image4 from '../img/ban mo 4.png'
 
 import { FaAngleRight } from "react-icons/fa6";
 import { FaAngleLeft } from "react-icons/fa6";
@@ -26,11 +25,11 @@ const BannerProduct = () => {
     ]
 
     const mobileImages = [
-        image1Mobile,
-        image2Mobile,
-        image3Mobile,
-        image4Mobile,
-        image5Mobile
+        image1,
+        image2,
+        image3,
+        image4
+
     ]
 
     const nextImage = () => {
