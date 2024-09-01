580af6bc27c682a7344a9193f6f4b4fc2a169763
kishorek0344 <156921551+kishorek0344@users.noreply.github.com>
2024-09-01 10:32:29 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,16 +12,19 @@ const Home = () => {
 
       <HorizontalCardProduct category={"dress"} heading={"Todsy's Top dress"}/>
       <HorizontalCardProduct category={"shoes"} heading={"Popular Shoe's"}/>
-
+      <HorizontalCardProduct category={"watches"} heading={"watches"}/>
       <VerticalCardProduct category={"mobiles"} heading={"Mobiles"}/>
       <VerticalCardProduct category={"Mouse"} heading={"Mouse"}/>
+      <VerticalCardProduct category={"speakers"} heading={"Bluetooth Speakers"}/>
+      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"}/>
       <VerticalCardProduct category={"chain"} heading={"chain"}/>
       <VerticalCardProduct category={"bracelet"} heading={"Bracelet"}/>
       <VerticalCardProduct category={"frames"} heading={"Photography and Frames"}/>
-      <VerticalCardProduct category={"speakers"} heading={"Bluetooth Speakers"}/>
       <VerticalCardProduct category={"gifts"} heading={"Gifts"}/>
-      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"}/>
       <VerticalCardProduct category={"gadget"} heading={"Gadget"}/>
+      <VerticalCardProduct category={"electronic and home applicances"} heading={"Electronic and Home Applicances"}/>
+      <VerticalCardProduct category={"kitchen ware"} heading={"Kitchen Ware's"}/>
+      <VerticalCardProduct category={"furniture"} heading={"Furniture's"}/>
 
     </div>
   )
